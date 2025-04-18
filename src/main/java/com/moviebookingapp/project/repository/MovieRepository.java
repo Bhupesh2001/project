6634f7b3b5c5861a28ddbeab7b384ae3945e1956
Bhupesh <53337976+Bhupesh2001@users.noreply.github.com>
2025-04-18 21:35:52 +0530

    refactor: update repositories to use MongoRepository for consistency

--- src/main/java/com/moviebookingapp/project/repository/MovieRepository.java
+++ src/main/java/com/moviebookingapp/project/repository/MovieRepository.java
@@ -1,10 +1,10 @@
 package com.moviebookingapp.project.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.moviebookingapp.project.entity.Movie;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, String> {
+public interface MovieRepository extends MongoRepository<Movie, String> {
 }
--- src/main/java/com/moviebookingapp/project/repository/UserRepository.java
+++ src/main/java/com/moviebookingapp/project/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.moviebookingapp.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.moviebookingapp.project.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
